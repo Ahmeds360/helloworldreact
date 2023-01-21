664397ec71a9f767e5520378cf6f13e02aee1335
Ahmad Babar <ahmadbabar8@icloud.com>
2023-01-20 22:13:29 -0500

    Added darkmore and responsive and dovker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
-
+import { BsFillMoonFill } from 'react-icons/bs';
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <>
       <Head>
@@ -13,8 +15,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col items-center justify-center h-screen">
-        <h1 className=" text-7xl font-bold font-inter hover:text-9xl ease-in-out duration-300  ">Hello, World!</h1>
+      {/* <BsFillMoonFill className="absolute top-0 right-0 m-4 text-4xl" /> */}
+
+      <main className="flex flex-col items-center justify-center h-screen dark:bg-black">
+        <h1 className=" text-5xl font-bold font-inter hover:text-6xl ease-in-out duration-300 md:text-7xl md:hover:text-9xl dark:text-white">
+          Hello, World!
+        </h1>
       </main>
     </>
   );
