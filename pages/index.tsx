89a0a7b43b96db1325e90bf4b5d47dd233b7d6df
Ahmeds360 <ahmadbabar8@icloud.com>
2023-04-01 15:28:28 -0400

    Ultra optoized for speed and ease of use

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { BsFillMoonFill } from 'react-icons/bs';
 import { useState } from "react";
 
 export default function Home() {
@@ -18,10 +17,12 @@ export default function Home() {
       {/* <BsFillMoonFill className="absolute top-0 right-0 m-4 text-4xl" /> */}
 
       <main className="flex flex-col items-center justify-center h-screen dark:bg-black">
-        <h1 className=" text-5xl font-bold font-inter hover:text-6xl ease-in-out duration-300 md:text-7xl md:hover:text-9xl dark:text-white">
+        <h1 className=" text-6xl font-bold font-inter hover:text-7xl ease-in-out duration-300 md:text-8xl md:hover:text-9xl sm:text-7xl sm:hover:text-8xl dark:text-white lg:text-[140px] lg:hover:text-[165px] xl:text-[200px] xl:hover:text-[215px] 2xl:text-[240px]  2xl:hover:text-[265px]">
           Hello, World!
         </h1>
       </main>
     </>
   );
 }
+
+//sec
